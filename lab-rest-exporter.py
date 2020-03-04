08ae7700705b62a2f71edbb99d4d4b94730fa833
andersla <anders@ormbunkar.se>
2020-03-04 14:06:32 +0100

    fixing rest-exporter

--- lab-rest-exporter.py
+++ lab-rest-exporter.py
@@ -5,21 +5,27 @@ import time
 import prometheus_client
 from prometheus_client import Gauge
 import requests
+import json
 
 
 # this might be the better way to code: https://github.com/prometheus/client_python/blob/master/README.md
 
-UPDATE_PERIOD = 10 # in sec
+UPDATE_PERIOD = 2 # in sec
 
 SHAKER_STATUS_GAUGE = prometheus_client.Gauge('shaker',
                                              'Shaker status API',
                                              ['location'])
 
 
-def getShakerStatus(SHAKER_STATUS_GAUGE, location, serial_device):
-  url = 'http://washer.lab.pharmb.io:6000/is_ready'
-  response = requests.get(url)
-  logging.info(str(response))
+def getShakerStatus(gauge, location):
+  url = 'http://washer.lab.pharmb.io:5000/is_ready'
+  response = requests.get(url, timeout=1)
+  logging.info(str(response.content))
+  data = json.loads(response.content)
+  logging.info(str(data))
+  value = data['value']
+  logging.info(str(value))
+  gauge.labels(location).set(value)
 
 
 if __name__ == '__main__':
@@ -30,7 +36,7 @@ if __name__ == '__main__':
                             datefmt='%H:%M:%S',
                             level=logging.INFO)
 
-  prometheus_client.start_http_server(10001)
+  prometheus_client.start_http_server(10002)
 
   while True:
     logging.info("Hello")
