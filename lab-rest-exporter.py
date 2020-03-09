c9aaef57a2dd4e0ce257363397ecb67ed404d379
andersla <anders@ormbunkar.se>
2020-03-09 08:51:11 +0100

    rest exporter and systemd services

--- lab-rest-exporter.py
+++ lab-rest-exporter.py
@@ -10,22 +10,36 @@ import json
 
 # this might be the better way to code: https://github.com/prometheus/client_python/blob/master/README.md
 
-UPDATE_PERIOD = 2 # in sec
+UPDATE_PERIOD = 60 # in sec
 
+SHAKER_URL = 'http://shaker.lab.pharmb.io:5000/is_ready'
 SHAKER_STATUS_GAUGE = prometheus_client.Gauge('shaker',
                                              'Shaker status API',
                                              ['location'])
 
+INCUBATOR_URL = 'http://incubator.lab.pharmb.io:5001/is_ready'
+INCUBATOR_STATUS_GAUGE = prometheus_client.Gauge('incubator',
+                                             'Incubator status API',
+                                             ['location'])
 
-def getShakerStatus(gauge, location):
-  url = 'http://washer.lab.pharmb.io:5000/is_ready'
+WASHER_URL = 'http://washer.lab.pharmb.io:5000/is_ready'
+WASHER_STATUS_GAUGE = prometheus_client.Gauge('washer',
+                                             'Washer status API',
+                                             ['location'])
+
+DISPENSER_URL = 'http://dispenser.lab.pharmb.io:5001/is_ready'
+DISPENSER_STATUS_GAUGE = prometheus_client.Gauge('dispenser',
+                                             'Dispenser status API',
+                                             ['location'])
+
+def getIsReadyValue(gauge, url, location):
   response = requests.get(url, timeout=1)
-  logging.info(str(response.content))
+  logging.debug(str(response.content))
   data = json.loads(response.content)
-  logging.info(str(data))
   value = data['value']
-  logging.info(str(value))
-  gauge.labels(location).set(value)
+  boolValue = int(value == 'True')
+  logging.debug(str(gauge.describe()) + " = " + str(url) + " = " + str(boolValue))
+  gauge.labels(location).set(boolValue)
 
 
 if __name__ == '__main__':
@@ -34,14 +48,17 @@ if __name__ == '__main__':
   #
   logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                             datefmt='%H:%M:%S',
-                            level=logging.INFO)
+                            level=logging.DEBUG)
 
   prometheus_client.start_http_server(10002)
 
   while True:
     logging.info("Hello")
     try:
-      getShakerStatus(SHAKER_STATUS_GAUGE, 'inside')
+      getIsReadyValue(SHAKER_STATUS_GAUGE, SHAKER_URL, 'inside')
+      getIsReadyValue(INCUBATOR_STATUS_GAUGE, INCUBATOR_URL, 'inside')
+      getIsReadyValue(WASHER_STATUS_GAUGE, WASHER_URL, 'inside')
+      getIsReadyValue(DISPENSER_STATUS_GAUGE, DISPENSER_URL, 'inside')
     except Exception as e:
       print(traceback.format_exc())
       logging.error(e)
